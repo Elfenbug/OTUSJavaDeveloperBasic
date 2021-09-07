edebeaa952dc35e924d4f8f973669aa0e853b57a
Elfenbug <elfenbug@gmail.com>
2021-09-08 00:52:23 +0500

    change List -> Map commit

--- HW06/src/ru/otus/Main.java
+++ HW06/src/ru/otus/Main.java
@@ -1,17 +1,26 @@
 package ru.otus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Bank bank = new Bank();
         Client vasya = new Client("Вася",18);
         Client masha = new Client("Маша",19);
-        Bank bank = new Bank();
+        List<Account> accountVasya = new ArrayList<>();
+        List<Account> accountMasha = new ArrayList<>();
 
-        bank.newAccount("new", 10, vasya);
-        bank.newAccount("new2", 300, vasya);
-        bank.newAccount("old", 10, masha);
+
+        bank.newAccount(vasya,"new", 10);
+        bank.newAccount(vasya,"new2", 300);
+        bank.newAccount(masha,"old", 10);
+        System.out.println("=========Список всех счетов с клиентами===========");
         System.out.println(bank);
+        System.out.println("===========Поиск клиентов по клиенту===========");
         System.out.println(bank.seekClient("new"));
         System.out.println(bank.seekClient("old"));
+        System.out.println("============Поиск счета по клиенту=============");
         System.out.println(bank.seekAccount(vasya));
 
     }
